764f8104bd08aed58fc4699820cedc5d4aef6079
Eric Lavagni <Eric.Lavagni@tlc.texas.gov>
2021-04-02 21:17:49 -0500

    Add code for App.js, bootstrap.js, index.js, and index.html (very similar to the code for marketing)

---
+++ packages/auth/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'au'
+})
+
+export default ({ history }) => {
+    return (
+    <div>
+        <StylesProvider generateClassName= {generateClassName} >
+            <Router history={history}>
+                <Switch>
+                    <Route path="/auth/signin" component={Signin} />
+                    <Route path="/auth/signup"component={Signup} />
+                </Switch>
+            </Router>
+        </StylesProvider>
+    </div>
+    );
+}
