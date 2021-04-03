a47bd8cbbb568cb97d553aa6db8af76349ee929c
Eric Lavagni <Eric.Lavagni@tlc.texas.gov>
2021-04-02 20:51:51 -0500

    Replace the BrowserRouter with a router that uses a memory history object, also allow the history object to be passed as a prob. Also propagate navigation events down (from Container to MarketingApp) or up ( from MarketingApp to Container)

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,14 +1,34 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     // Use the useEffect hook to make sure the code gets executed only one time
-    // when the component if first displayed
-    useEffect(() => {
-        mount(ref.current);
-    });
+    // when the component if first displayed (by providing an empty dependency list)
+    useEffect(() => {        
+        //Call the mount function from MarketingApp, and pass an options object as the second argument
+        const { onParentNavigate } = mount(ref.current, {  
+            //the onNavigate callback will give us the navigation object, we just need the pathname, so we can 
+            //deconstruct and rename the property to nextPathname          
+            onNavigate: ({ pathname: nextPathname }) => {
+                //When onNavigate is called we will sync the container browser history with values from Marketing Memory History (url paths)
+                //(propage navigation event up)
+                const { pathname }  = history.location;
+
+               //Only navigate if the two paths are different, if they are the same and we perform
+               //the navidation, it would lead to an infinive loop (circular navigation)
+               if (pathname !== nextPathname) {
+                   //navigate to path
+                   history.push(nextPathname);  
+               }               
+            }
+        });
+
+        history.listen(onParentNavigate);
+    }, []);
 
     return <div ref={ref} />
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 
 //Mount function to start up the app
-const mount = (el) => {
-    ReactDOM.render(<App/>, el);   
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+    //defaultHistory is only provided if we are running in dev in isolation
+    const history = defaultHistory || createMemoryHistory({
+        initialEntries: [initialPath]
+    });
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }   
+
+    ReactDOM.render(<App history={history} />, el);   
+
+    return  {
+        //return this function when mount is called, so that the container can
+        //notify the MarketingApp when navigation occurs (propagate event down)
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+            
+            if(pathname !== nextPathname) {
+                history.push(location);
+            }            
+        },
+    };
 };
 
 //If we are in development and in isolation, 
@@ -14,7 +36,7 @@ if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
